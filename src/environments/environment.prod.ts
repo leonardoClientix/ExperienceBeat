fb5c0ed1cf3360fc3aa7e002ebd168f61317288c
leonardoClientix <leonardo.hernandez@clientix.co>
2020-05-05 15:06:32 -0500

    Pasar firebase a produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBmEyOSY2atjD7SEcWDURpMBTdMKxS20l8",
+    authDomain: "encuestas-a0bd4.firebaseapp.com",
+    databaseURL: "https://encuestas-a0bd4.firebaseio.com",
+    projectId: "encuestas-a0bd4",
+    storageBucket: "encuestas-a0bd4.appspot.com",
+    messagingSenderId: "568247986243",
+    appId: "1:568247986243:web:22e0b7aa8739173ee74cf6",
+    measurementId: "G-MTZXSJ78TK"
+
+    /*apiKey: "AIzaSyA1PM5iboGgbXiQQLWtvZVG0V7clOVHcG4",
+    authDomain: "encuesta2-8f4b7.firebaseapp.com",
+    databaseURL: "https://encuesta2-8f4b7.firebaseio.com",
+    projectId: "encuesta2-8f4b7",
+    storageBucket: "encuesta2-8f4b7.appspot.com",
+    messagingSenderId: "383615728839",
+    appId: "1:383615728839:web:69d9e33e4e1dddfd1752e2",
+    measurementId: "G-M28CZWSJ8C"*/
+  }
 };
